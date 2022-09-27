077260ebc4378f80c357fd48266f0b4f0e095fce
takuma-ru <kcattdev@gmail.com>
2022-09-28 07:56:23 +0900

    [Update] Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 const IndexMain = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+
   .title {
-    
+    text-align: center;
+
+    h1 {
+      margin: 0px;
+
+      font-size: 5rem;
+    }
+
+    p {
+      font-size: 1.5rem;
+    }
   }
 `
 
@@ -17,7 +34,27 @@ const Index: NextPage = () => {
     </Head>
 
     <IndexMain>
-      asd
+      <motion.div
+        initial={{
+          opacity: 0,
+          scale: 0
+        }}
+        animate={{
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          type: 'spring',
+          delay: 0.5,
+          stiffness: 260,
+          damping: 16
+        }}
+        className="title"
+      >
+        <h1>一関高専</h1>
+        <h1>電子計算機部</h1>
+        <p>- 指先から世界を変える -</p>
+      </motion.div>
     </IndexMain>
   </>
 }
